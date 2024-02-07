e25c0cba4e5c44f0a74ece9e752e28f61554eb9f
armangracia <graciaarmansteven@gmail.com>
2024-02-08 01:40:04 +0800

    login page frontend

--- frontend/src/Components/Layouts/Metadata.js
+++ frontend/src/Components/Layouts/Metadata.js
@@ -4,7 +4,7 @@ import { Helmet } from 'react-helmet'
 const MetaData = ({ title }) => {
     return (
         <Helmet>
-            <title>{`${title} - BitterGourd`}</title>
+            <title>{`${title} - Youth Empowerment Hub `}</title>
         </Helmet>
     )
 }
--- frontend/src/Components/User/Login.js
+++ frontend/src/Components/User/Login.js
@@ -3,10 +3,11 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import Metadata from '../Layouts/Metadata';
 import Loader from '../Layouts/Loader';
 import axios from 'axios';
-import { toast } from 'react-toastify';
+
+import 'boxicons'
+import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { authenticate, getUser } from '../../utils/helpers';
-import { ToastContainer } from 'react-toastify';
 
 import '../Layouts/FH.css';
 import '../Layouts/RLForms.css';
@@ -15,14 +16,21 @@ const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
-
   const navigate = useNavigate();
-  let location = useLocation();
+  const location = useLocation();
   const redirect = location.search ? new URLSearchParams(location.search).get('redirect') : '';
-  const notify = (error) =>
+
+  useEffect(() => {
+    if (getUser() && redirect === 'shipping') {
+      navigate(`/${redirect}`);
+    }
+  }, [redirect, navigate]);
+
+  const notify = (error) => {
     toast.error(error, {
       position: toast.POSITION.BOTTOM_RIGHT,
     });
+  };
 
   const login = async (email, password) => {
     try {
@@ -36,9 +44,7 @@ const Login = () => {
       authenticate(data, () => navigate('/'));
       window.location.reload();
     } catch (error) {
-      toast.error('Invalid user or password', {
-        position: toast.POSITION.BOTTOM_RIGHT,
-      });
+      notify('Invalid user or password');
     }
   };
 
@@ -47,37 +53,54 @@ const Login = () => {
     login(email, password);
   };
 
-  useEffect(() => {
-    if (getUser() && redirect === 'shipping') {
-      navigate(`/${redirect}`);
-    }
-  }, []);
-
   return (
     <Fragment>
       {loading ? (
         <Loader />
       ) : (
         <Fragment>
-          {/* <Metadata title={'Login'} /> */}
           <div className="container-login">
             <div className="content">
-              <h2 class="logo"><span class="bx--bxl-firebase"> Youth Empowerment Hub</span></h2>
-
-              <div class="text-sci">
+              <h2 className="logo"><span className="healthicons--pregnant-outline"> Youth Hub</span></h2>
+              <div className="text-sci">
                 <h2>Welcome!<br /><span>To our Website.</span></h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum, velit eget fermentum consectetur, erat ex sodales sapien, vel rutrum odio dui at velit. Nulla facilisi. Integer consectetur lorem nec libero bibendum, sit amet dapibus turpis hendrerit.</p>
-
-                <div className="social-icons">
-                  <a href="#"><i className='bx bxl-linkedin'></i></a>
-                  <a href="#"><i className='bx bxl-facebook'></i></a>
-                  <a href="#"><i className='bx bxl-instagram'></i></a>
-                  <a href="#"><i className='bx bxl-twitter'></i></a>
-                </div>
+                <p>Lorem ipsum dolor sit amhiet, consectetur adipiscing elit.<br></br> Sed vestibulum, velit eget fermentum consectetur, erat ex sodales sapien, vel rutrum odio dui at velit.<br></br> Nulla facilisi. Integer consectetur lorem nec libero bibendum, sit amet dapibus turpis hendrerit.
+                </p>
               </div>
             </div>
-            <div className="logreg-box"></div>
+            <div className="logreg-box">
+              <div className="form-box login">
+                <form onSubmit={submitHandler}>
+                  <h2>Sign In</h2>
+
+                  <div className="input-box">
+                  <span class="icon"><box-icon type='solid' name='envelope'></box-icon>
+                    </span>
+                    <input type="email" required />
+                    <label>Email</label>
+                  </div>
+
+                  <div className="input-box">
+                    <span class="icon"><box-icon type='solid' name='lock-alt'></box-icon></span>
+                    <input type="password" required />
+                    <label>Password</label>
+                  </div>
+                  <div class="remember-forgot">
+                    <label>
+                      <input type="checkbox" />
+                      Remember me
+                    </label>
+                    <a href="#">Forgot Password</a>
+                  </div>
+
+                  <button type="submit" class="btn">Login</button>
+
+                  <div class="login-register">
+                    <p>Don't have an account? <a href="#" class="register-link">Sign Up</a></p>
+                  </div>
+                </form>
+              </div>
+            </div>
           </div>
           <ToastContainer />
         </Fragment>
